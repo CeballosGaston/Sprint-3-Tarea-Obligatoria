fcdabbc0de791f7fcb182cdd7cb2b20ad2ffa6f9
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-02-09 10:40:41 +0100

    Variables added

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
+let currentPage = 1;
+const itemsPerPage = 10;
+
+const apiSelector = document.getElementById("fetchOrAxios");
+const searchInput = document.getElementById("searchInput");
+const fetchButton = document.getElementById("getDataBtn");
+const loadingElement = document.getElementById("loading");
+const errorElement = document.getElementById("error");
+const resultsContainer = document.getElementById("results");
+const paginationContainer = document.getElementById("paginationContainer");
+const numberButtons = document.getElementById("number-btns");
+
